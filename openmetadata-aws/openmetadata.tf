e1bcac315c3dc4c50f555eaab5d03adf4d1ca5f0
Andrés Muñoz <andres@getcollate.io>
2024-11-28 18:50:29 +0100

    GEN-1521 submodule instances pointing to version 1.5.12

--- openmetadata-aws/openmetadata.tf
+++ openmetadata-aws/openmetadata.tf
@@ -1,5 +1,5 @@
 module "deployment" {
-  source = "github.com/open-metadata/openmetadata-terraform//submodules/openmetadata-deployment?ref=GEN-1521-aws-initial-version"
+  source = "github.com/open-metadata/openmetadata-terraform//submodules/openmetadata-deployment?ref=1.5.12"
 
   namespace          = local.omd.namespace
   helm_chart_version = local.omd.helm_chart_version
--- openmetadata-aws/openmetadata_dependencies.tf
+++ openmetadata-aws/openmetadata_dependencies.tf
@@ -1,5 +1,5 @@
 module "openmetadata_deps" {
-  source = "github.com/open-metadata/openmetadata-terraform//submodules/openmetadata-dependencies?ref=GEN-1521-aws-initial-version"
+  source = "github.com/open-metadata/openmetadata-terraform//submodules/openmetadata-dependencies?ref=1.5.12"
 
   namespace          = local.omd.namespace
   helm_chart_version = local.omd.helm_chart_version
