f2769f38d5643403060e54e305da69662103768c
akash-jain-10 <15995028+akash-jain-10@users.noreply.github.com>
2025-07-31 05:20:01 +0000

    chore(release): Prepare Branch for release 1.8.8

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "app_namespace" {
 variable "app_version" {
   type        = string
   description = "OpenMetadata version to deploy."
-  default     = "1.8.7"
+  default     = "1.8.8"
 }
 
 variable "docker_image_name" {
