f9fe21e5288f69ed0e76728e18c3610c3f92beae
akash-jain-10 <15995028+akash-jain-10@users.noreply.github.com>
2025-08-05 11:40:19 +0000

    chore(release): Prepare Branch for release 1.8.10

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -66,7 +66,7 @@ variable "app_extra_envs" {
 variable "app_version" {
   type        = string
   description = "OpenMetadata version to deploy"
-  default     = "1.8.9"
+  default     = "1.8.10"
 }
 
 variable "azs_to_use" {
--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "app_namespace" {
 variable "app_version" {
   type        = string
   description = "OpenMetadata version to deploy."
-  default     = "1.8.9"
+  default     = "1.8.10"
 }
 
 variable "docker_image_name" {
