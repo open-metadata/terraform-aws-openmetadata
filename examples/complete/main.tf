fdff8adbba29d34e3e402f1ab3b485985bd6d012
akash-jain-10 <15995028+akash-jain-10@users.noreply.github.com>
2025-09-01 04:26:34 +0000

    chore(release): Prepare Branch for release 1.9.5

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,7 +2,7 @@
 
 module "app" {
   source  = "open-metadata/openmetadata/aws"
-  version = "1.9.4"
+  version = "1.9.5"
 
   airflow  = var.airflow
   env_from = local.env_from
--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "app_namespace" {
 variable "app_version" {
   type        = string
   description = "OpenMetadata version to deploy."
-  default     = "1.9.4"
+  default     = "1.9.5"
 }
 
 variable "docker_image_name" {
