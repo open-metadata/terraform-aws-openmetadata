8eff58ff9d5c0fcb58ff5c9abe7b10797514263b
tutte <2269035+tutte@users.noreply.github.com>
2025-11-17 09:23:08 +0000

    chore(release): Prepare Branch for release 1.10.7

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "app_namespace" {
 variable "app_version" {
   type        = string
   description = "OpenMetadata version to deploy."
-  default     = "1.10.6"
+  default     = "1.10.7"
 }
 
 variable "docker_image_name" {
