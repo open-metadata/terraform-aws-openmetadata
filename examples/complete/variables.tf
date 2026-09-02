f4dd85ce0afec9e83e2d3eefca3ec783f627547b
pellejador <1035091+pellejador@users.noreply.github.com>
2026-09-02 10:48:34 +0000

    chore(release): Prepare Branch for release 2.0.1

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -66,7 +66,7 @@ variable "app_extra_envs" {
 variable "app_version" {
   type        = string
   description = "OpenMetadata version to deploy"
-  default     = "2.0.0"
+  default     = "2.0.1"
 }
 
 variable "azs_to_use" {
